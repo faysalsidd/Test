5bd60117d7a090cd5951e2d69f628200fe0362ae
faysalsidd <faysal_sidd@live.ca>
2017-08-10 19:26:17 -0400

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = AMethod(true, true, false);
+            Console.WriteLine(a);
+        }
+
+        private static int AMethod(bool first, bool second, bool third)
+        {
+            int OptionOne = 1;
+            int OptionTwo = 2;
+
+            if (!first && second)
+            {
+                return OptionOne;
+            }
+            else if ((!second) || (!third) || second )
+            {
+                return OptionTwo;
+           
+            }
+            else
+                 return OptionOne;
+
+        }
+    }
+}
